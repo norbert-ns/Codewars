767902f2f298db798483541e2fbd42200e976f4e
Norbert Nsengiyumva <77836231+norbert-ns@users.noreply.github.com>
2026-05-11 12:50:45 +0200

    Add solution - CodeHub

---
+++ 8-kyu/triple-trouble-2/triple-trouble-2.js
@@ -0,0 +1,10 @@
+function tripleTrouble(one, two, three){
+  let result = "";
+  
+  for (let i = 0; i < one.length; i++) {
+    result += one[i] + two[i] + three[i];
+  }
+  
+  return result;
+}
+​
